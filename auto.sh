c72084e93d86cb63c138a34c68f837e07aebe081
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-09 15:38:30 +0800

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Update packages
+sudo apt-get update
+
+# Install vsftpd
+sudo apt-get install vsftpd -y
+
+# Backup the original vsftpd configuration file
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.orig
+
+# Create the FTP user
+sudo useradd -m relay -s /usr/sbin/nologin
+echo "relay:Sadri@123" | sudo chpasswd
+
+# Configure vsftpd
+echo "listen=NO
+listen_ipv6=YES
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+use_localtime=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+chroot_local_user=YES
+secure_chroot_dir=/var/run/vsftpd/empty
+pam_service_name=vsftpd
+rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+ssl_enable=NO
+pasv_enable=Yes
+pasv_min_port=40000
+pasv_max_port=40100
+allow_writeable_chroot=YES
+local_root=/root" | sudo tee /etc/vsftpd.conf
+
+# Restart vsftpd
+sudo service vsftpd restart
