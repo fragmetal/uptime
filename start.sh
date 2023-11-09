59bdf7d69edb746ff70dc7126ff9ff174569753c
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-09 13:49:52 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/louislam/uptime-kuma.git && \
+cd uptime-kuma && npm install npm@9 -g && npm run setup && screen -dmS uptime-kuma node server/server.js && \
+cloudflared service install eyJhIjoiN2Q4ZGI3YTgzODU5MjQxZDdmMDI4ZmM2MjhkOTcxNmMiLCJ0IjoiMmYzMWQ2NTItN2IwNS00Mzc1LTliYzEtYmI4OGNiYmY1MjU4IiwicyI6Ik4yVXdaREl5TkRRdFpXUmpOaTAwWTJZeExUaGpaREV0TURVM05EbG1ZekJpTnpkbCJ9
