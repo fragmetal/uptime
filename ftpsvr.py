a17bbb13885ae3d6ce30b358aa4252ab3df3863f
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-09 16:13:17 +0800

    Delete ftpsvr.py

--- ftpsvr.py
+++
@@ -1,7 +0,0 @@
-from pyngrok import ngrok
-
-# Set your authtoken from your ngrok account
-ngrok.set_auth_token("26eB9yvckCFT8fpaZMsXfRnES3c_59yZgjPQPH4Dt6G3MXmGN")
-
-tunnel = ngrok.connect(21, "tcp")
-print("Public Access:", tunnel.public_url)
