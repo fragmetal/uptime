9f7df953804e4a9092aae4d9086eb7c1a4e71d80
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-09 15:53:39 +0800

    Update ftpsvr.py

--- ftpsvr.py
+++ ftpsvr.py
@@ -1,24 +1,7 @@
-from twisted.protocols.ftp import FTPFactory, FTPRealm
-from twisted.cred.portal import Portal
-from twisted.cred.checkers import InMemoryUsernamePasswordDatabaseDontUse
-from twisted.internet import reactor
 from pyngrok import ngrok
 
 # Set your authtoken from your ngrok account
 ngrok.set_auth_token("26eB9yvckCFT8fpaZMsXfRnES3c_59yZgjPQPH4Dt6G3MXmGN")
 
-tunnel = ngrok.connect(2121, "tcp")
+tunnel = ngrok.connect(21, "tcp")
 print("Public Access:", tunnel.public_url)
-
-# Create your username and password.
-checkers = [InMemoryUsernamePasswordDatabaseDontUse(relay='Sadri@123')]
-
-# Create a portal and register checkers with it.
-p = Portal(FTPRealm('./'), checkers)
-
-# Create an FTP factory and register the portal with it.
-factory = FTPFactory(p)
-
-# Start the FTP server on localhost at port 2121.
-reactor.listenTCP(2121, factory)
-reactor.run()
