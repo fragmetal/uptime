3016ee609e4537f9c7f9afa0e86a46676c739b60
Sadri <108393507+fragmetal@users.noreply.github.com>
2023-11-09 14:47:21 +0800

    Update ftpserver.py

--- ftpserver.py
+++ ftpserver.py
@@ -2,6 +2,13 @@ from twisted.protocols.ftp import FTPFactory, FTPRealm
 from twisted.cred.portal import Portal
 from twisted.cred.checkers import InMemoryUsernamePasswordDatabaseDontUse
 from twisted.internet import reactor
+from pyngrok import ngrok
+
+# Set your authtoken from your ngrok account
+ngrok.set_auth_token("26eB9yvckCFT8fpaZMsXfRnES3c_59yZgjPQPH4Dt6G3MXmGN")
+
+tunnel = ngrok.connect(2121, "tcp")
+print("Public URL:", tunnel.public_url)
 
 # Create your username and password.
 checkers = [InMemoryUsernamePasswordDatabaseDontUse(relay='Sadri@123')]
